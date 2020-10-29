a6bf581257360be8b6dccf33cebe7fef6c58b1e9
royhnoels <noelodawa286@gmail.com>
2020-10-29 12:24:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-1jbwh9
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-1jbwh9)
---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    name: "chelsea boot",
+    price: 799,
+    description: "A leather shoe made from italy"
+  },
+  {
+    name: "timber land boot",
+    price: 699,
+    description: "A great genuine leather boot "
+  },
+  {
+    name: "leorpad",
+    price: 299,
+    description: "a sleek nice simple rubber shoe currently in fashion"
+  }
+];
